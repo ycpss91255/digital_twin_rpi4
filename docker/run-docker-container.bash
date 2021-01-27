10ca531a4cc6dfc382245b4b7d35d7748a1f1db1
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-01-27 12:47:33 +0800

    init : environment init

---
+++ docker/run-docker-container.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# start sharing xhost
+xhost +local:root
+
+# run docker
+docker run --rm \
+  --net=host \
+  --ipc=host \
+  --privileged \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:$docker/.Xauthority \
+  -v $HOME/workspace/digital_twin_rpi4_ws:$HOME/work \
+  -e XAUTHORITY=$home_folder/.Xauthority \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -it --name "digital-twin-rpi4" $(id -un)/digital-twin-rpi4
+
+
