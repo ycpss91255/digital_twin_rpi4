698ad90cfb293306ff5a966cddbcf1a754f87d5b
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-02-01 13:52:33 +0800

    fix : docker image source

--- docker/build-dokcer-image.bash
+++ docker/build-dokcer-image.bash
@@ -8,6 +8,8 @@ group=`id -gn`
 uid=`id -u`
 gid=`id -g`
 
+export DOCKER_BUILDKIT=1
+
 # build docker images
 docker build -t ${user}/digital-twin-rpi4 \
     --build-arg USER=${user} \
--- docker/run-docker-container.bash
+++ docker/run-docker-container.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start sharing xhost
-xhost +local:root
+# xhost +local:root
 
 # run docker
 docker run --rm \
