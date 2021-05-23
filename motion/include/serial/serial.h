52c586b2d14fbb2ec951fa4cacb831e1f7490cf3
YUN-CHIEN CHEN <ycpss91255@gmail.com>
2021-05-23 20:24:34 +0800

    add : motion and UART related stuff

---
+++ motion/include/serial/serial.h
@@ -0,0 +1,99 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <errno.h> /* Error number definitions */
+#include <fcntl.h> /* File control definitions */
+#include <stdio.h> /* Standard input/output definitions */
+#include <stdlib.h>
+#include <string.h>  /* String function definitions */
+#include <termios.h> /* POSIX terminal control definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
+
+#include <cmath>
+#include <string>
+
+// #define DEBUG
+
+using namespace std;
+
+class Serial {
+  // functions
+ public:
+  Serial(const char*, unsigned int);
+  ~Serial();
+
+ public:
+  /*
+  *@param float [] min to max
+
+  * public UART data:
+
+  * start packat: 0xAA
+
+  * 4 motor direction(1 byte ): {4'b0000, MD(1bit), MB(1bit), MC(1bit),
+  MA(1bit)}
+
+  * 4 motor speed    (8 bytes): {MA(short_uint_2bytes), MB(short_uint_2bytes),
+  MC(short_uint_2bytes), MD(short_uint_2bytes)}
+
+  * CRC (1 byte ): after adding the above 9 bytes, use the lowest byte as the
+  check code
+
+  * end packat       (1 byte ): 0xEE
+
+  * total 12 bytes
+  */
+  // AA FF FF FF EE;
+  void pub_motor_pwm(float[]);
+
+/* Uart資料:
+* 起始封包(1byte): 0xAA
+* 4顆馬達方向(1bytes): {4'b0000, MD(1bit), MB(1bit), MC(1bit), MA(1bit)}
+
+* 4顆馬達Encoder(16bytes): MA(int_4bytes), MB(int_4bytes), MC(int_4bytes),
+MD(int_4bytes)
+
+* 4顆馬達速度(8bytes): MA(short_uint_2bytes), MB(short_uint_2bytes),
+MC(short_uint_2bytes), MD(short_uint_2bytes)
+
+* 4顆馬達電壓(8bytes): MA(short_uint_2bytes), MB(short_uint_2bytes),
+MC(short_uint_2bytes), MD(short_uint_2bytes)
+
+* 4顆馬達電流(8bytes): MA(short_uint_2bytes), MB(short_uint_2bytes),
+MC(short_uint_2bytes), MD(short_uint_2bytes)
+
+* CRC(1byte): 以上41bytes相加後取最低那個Byte當檢查碼
+
+* 結束位元(1byte): 0xEE
+
+* 總共 44bytes
+* */
+  void sub_feedback();
+
+ private:
+  void build_msg(float[]);
+  void unbuild_msg();
+  // variables
+ public:
+ private:
+  const char* port = nullptr;
+  int fd;
+  termios opt;
+  // pub msg and default values is zero
+  char pub_msg[12] = {0};
+  // custom pub start and end packat
+  char pub_start_buf = 0xAA;
+  char pub_end_buf = 0xEE;
+
+  // sub msg and default values is zero
+  char sub_msg[44] = {0};
+  // custom start and end packat
+  char sub_start_buf = 0xAA;
+  char sub_end_buf = 0xEE;
+
+#ifdef DEBUG
+  void printf_binary(const char*, char);
+#endif  // DEBUG
+};
+
+#endif  // SERIAL_H
---
+++ motion/src/serial/serial.cpp
@@ -0,0 +1,150 @@
+#include "serial/serial.h"
+
+Serial::Serial(const char* port, unsigned int baud) {
+  this->fd = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
+
+  if (this->fd == -1) {
+    perror("open_port: Unable to open /dev/ttyUSB0 - ");
+  } else {
+    tcgetattr(this->fd, &opt);  // get serial port config
+    fcntl(this->fd, F_SETFL, 0);
+
+    int speed_arr[] = {B0,    B50,   B75,    B110,   B134,   B150,
+                       B200,  B300,  B600,   B1200,  B1800,  B2400,
+                       B4800, B9600, B19200, B38400, B57600, B115200};
+    int name_arr[] = {0,    50,   75,    110,   134,   150,
+                      200,  300,  600,   1200,  1800,  2400,
+                      4800, 9600, 19200, 38400, 57600, 115200};
+    int status;
+    tcgetattr(this->fd, &opt);
+    for (int i = 0; i < sizeof(speed_arr) / sizeof(int); i++) {
+      if (baud == name_arr[i]) {
+        tcflush(this->fd, TCIOFLUSH);
+        cfsetispeed(&opt, speed_arr[i]);
+        cfsetospeed(&opt, speed_arr[i]);
+        status = tcsetattr(this->fd, TCSANOW, &opt);
+        if (status != 0) {
+          perror("tcsetattr fd");
+          return;
+        }
+        tcflush(this->fd, TCIOFLUSH);
+      }
+    }
+
+    opt.c_cflag |= (CLOCAL | CREAD);
+    opt.c_cflag &= ~PARENB;
+    opt.c_cflag &= ~CSTOPB;
+    opt.c_cflag &= ~CSIZE;
+    opt.c_cflag |= CS8;
+    opt.c_cflag &= ~CRTSCTS;
+    opt.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    opt.c_iflag &= ~INPCK;
+    opt.c_iflag &= ~(IXON | IXOFF | IXANY);
+    opt.c_oflag &= ~OPOST;
+
+    tcsetattr(this->fd, TCSANOW, &opt);
+  }
+}
+
+Serial::~Serial() { close(this->fd); }
+
+void Serial::build_msg(float pwm[4]) {
+  // write start and end packat
+  this->pub_msg[0] = this->pub_start_buf;
+  this->pub_msg[11] = this->pub_end_buf;
+
+  // pwm values process
+  bool dir[4] = {false};
+  int tmp_pwm[4] = {0};
+
+  for (int i = 0; i < 4; i++) {
+    dir[i] = pwm[i] > 0 ? 1 : 0;
+
+    // After obtaining dir, pwm is converted to unsigned
+    pwm[i] = abs(pwm[i]);
+    // speed 0 ~ 100 conversion to 0 ~ 65535
+    tmp_pwm[i] = pwm[i] > 100 ? 65535 : int(round((pwm[i]) * 655.35));
+    // tmp_pwm type = 2 bytes = 16 bits, but msg type is char(1 byte), so
+    // disassemble tmp_pwm into HIGH and Low byte
+    this->pub_msg[2 + i * 2] = (tmp_pwm[i] & 0xFF00) >> 8;
+    this->pub_msg[3 + i * 2] = tmp_pwm[i] & 0x00FF;
+  }
+
+  // write dir to msg[1] low bit
+  this->pub_msg[1] |= dir[0] << 0;
+  this->pub_msg[1] |= dir[2] << 1;
+  this->pub_msg[1] |= dir[1] << 2;
+  this->pub_msg[1] |= dir[3] << 3;
+
+  // CRC Calculation
+  int crc = 0;
+  for (int i = 0; i < 9; i++) {
+    crc += this->pub_msg[i + 1];
+    crc = crc & 0xFF;
+  }
+  this->pub_msg[10] = crc;
+
+#ifdef DEBUG
+  printf_binary("dir: ", this->msg[1]);
+  printf("\n");
+  printf_binary("MA H_Spd: ", this->msg[2]);
+  printf_binary("MA L_Spd: ", this->msg[3]);
+  printf("\n");
+  printf_binary("MB H_Spd: ", this->msg[4]);
+  printf_binary("MB L_Spd: ", this->msg[5]);
+  printf("\n");
+  printf_binary("MC H_Spd: ", this->msg[6]);
+  printf_binary("MC L_Spd: ", this->msg[7]);
+  printf("\n");
+  printf_binary("MD H_Spd: ", this->msg[8]);
+  printf_binary("MD L_Spd: ", this->msg[9]);
+  printf("\n");
+  printf_binary("CRC     : ", this->msg[10]);
+#endif  // DEBUG
+}
+
+void Serial::pub_motor_pwm(float pwm[4]) {
+  build_msg(pwm);
+
+  int n = write(this->fd, this->pub_msg, 12);
+}
+
+void Serial::unbuild_msg(){
+
+}
+
+void Serial::sub_feedback(){
+
+    // Read bytes. The behaviour of read() (e.g. does it block?,
+    // how long does it block for?) depends on the configuration
+    // settings above, specifically VMIN and VTIME
+    int n = read(this->fd, this->sub_msg, 44);
+
+    // if (n > 0) {
+    //   printf("n = %d, %s\n", n, read_buf);
+    //   for (int i = 0; i < read_len; i++) {
+    //     if (read_buf[i] == '\000') {
+    //       printf("bingo, i = %d\n", (i + 1));
+    //       break;
+    //     }
+
+    // }
+
+    // if (n < 0) {
+    //   printf("n = %d, read() of %d bytes failed!\n", n, read_len);
+    // }
+
+  }
+
+}
+
+#ifdef DEBUG
+void Serial::printf_binary(const char* text, char hex_msg) {
+  printf("%s", text);
+  for (int i = 7; i >= 0; i--) {
+    printf("%d", ((hex_msg >> i) & 0x01));
+    if (i == 4) printf(" ");
+  }
+  printf("\n");
+}
+#endif  // DEBUG
